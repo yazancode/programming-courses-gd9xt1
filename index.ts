c856bad3f3377defdd2665805145638f9e7f68d6
yazancode <yy7321442@gmail.com>
2021-11-04 16:42:34 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -3,5 +3,11 @@
 //###########################   Programming Courses  ###################
 //####################################################################
 
+let mynum: number;
 
+mynum = parseInt(prompt('please inter number'));
 
+if (mynum > 0) {
+  console.log('positive number');
+}
+console.log('the end');
